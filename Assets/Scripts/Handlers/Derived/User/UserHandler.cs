7d29a13b6ca88961971d4c29e57d1447a28ebfde
ogzzmert <ogzzmert@gmail.com>
2022-12-17 21:37:23 +0300

    .

--- Assets/Scripts/Handlers/Derived/User/UserHandler.cs
+++ Assets/Scripts/Handlers/Derived/User/UserHandler.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class UserHandler : MonoBehaviour
+public class UserHandler : Handler
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum ResourceType
     {
-        
+        Gold = 0,
+        Wood = 1,
+        Brick = 2,
+        Iron = 3,
+        Gem = 4
     }
-
-    // Update is called once per frame
-    void Update()
+    [field: SerializeField] private Belonging[] belongings;
+    
+    [Serializable]
+    protected class Belonging
     {
-        
+        public ResourceType resourceType;
+        public int amount;
+    }
+    Dictionary<ResourceType, int> resource = new Dictionary<ResourceType, int>();
+    protected override void initialize()
+    {
+        foreach(ResourceType resourceType in Enum.GetValues(typeof(ResourceType))) resource.Add(resourceType, 0);
+        foreach(Belonging belonging in belongings) resource[belonging.resourceType] += belonging.amount;
     }
 }
